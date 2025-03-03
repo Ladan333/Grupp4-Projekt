f4fc081842fc71c2c70d6ca8910fa1fb99af8ff5
Lillens <johanandreasson84@gmail.com>
2025-03-03 21:49:47 +0100

    här kommer mer till profil

--- profil.php
+++ profil.php
@@ -1,10 +1,20 @@
 <?php
 require("PDO.php");
+session_start();
 
 if(isset($_GET["source"]) && $_GET["source"] == "search"){
 $stmt = $pdo->prepare("SELECT * FROM users WHERE user_name = :user");
 $stmt->bindParam(":user", $_GET[""]);
 }
+else{
+    $stmt = $pdo->prepare("SELECT * FROM USERS WHERE user_name = :user");
+    $stmt->bindParam(":user", $_SESSION["username"]);
+    
+}
+
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 ?>
 
@@ -19,9 +29,16 @@ $stmt->bindParam(":user", $_GET[""]);
     <title>Document</title>
 </head>
 <body>
-    <?php 
-    echo $_GET["user_name"];
-    ?>
+    <ul>
+
+    <?php foreach ($result as $key): ?>
+    <li>
+        <?php echo $result?>
+    </li>
+        <?php endforeach?>
+        <?php var_dump($result)?>
+
+    </ul>
     
 </body>
 </html>
