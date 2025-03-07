057ef0eb8c2642538517790e51e6d95a71f0c9de
Rasmus K <Rasmus.kjell@hotmail.com>
2025-03-07 14:24:25 +0100

    Fixed commenting on another person profile so it redirects to your own page

--- AddComments.php
+++ AddComments.php
@@ -10,6 +10,10 @@ session_start();
                         $blog_id = $_POST["blog_id"];
                         $source = $_POST['source'] ?? 'blogwall.php';
 
+                        if ($source = 'profile.php') {
+                            $source = 'profile.php?user_name=' . $_SESSION['follow_username'];
+                        }
+
                         $stmt = $pdo->prepare( "INSERT INTO comments(commentContent, user_id, blog_id)
                                                        VALUES(:commentsContent, :userid, :blog_id) ");
                         
