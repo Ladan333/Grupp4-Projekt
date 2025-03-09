d2144be27fcbfe27275805223d688aaa3316550c
Markus J <markus-butlers@hotmail.com>
2025-03-09 10:18:13 +0100

    knappar för att ändra adminrättigheter

---
+++ is_admin.php
@@ -0,0 +1,21 @@
+<?php
+session_start(); 
+require 'PDO.php';
+
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $user_id = $_GET['id'];
+
+
+    $isAdmin = (isset($_GET['role']) && $_GET['role'] == 0) ? 1 : 0;
+
+  
+    $stmt = $pdo->prepare("UPDATE users SET role = ? WHERE id = ?");
+    $stmt->execute([$isAdmin, $user_id]);
+
+   
+    header("Location: admin_list.php");
+    exit;
+} else {
+    echo "Invalid request. User ID is missing.";
+    exit;
+}
