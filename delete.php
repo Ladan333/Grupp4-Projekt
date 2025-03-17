f1c92c181d006ecd031fdde77a0a48645788298c
Lillens <johanandreasson84@gmail.com>
2025-03-17 15:09:32 +0100

    funkar

--- delete.php
+++ delete.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 require 'PDO.php';
+require_once 'UserController.php';
+require_once 'UserDAO.php';
 
 //Delete post - ligger i blogwall
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['post_id'])){
