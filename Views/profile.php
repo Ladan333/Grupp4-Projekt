e38f990f997a827fb31b83ff016cfa94b8546f32
Lillens <johanandreasson84@gmail.com>
2025-03-18 16:05:01 +0100

    ändrat Session(id) till user_id rad 233 profile.php

--- Views/profile.php
+++ Views/profile.php
@@ -230,7 +230,7 @@ $_SESSION['follow_username'] = $result['user_name'];
 
 
 
-                        <?php if ($_SESSION['role'] == 1 || $post['user_id'] == $_SESSION['id']): ?>
+                        <?php if ($_SESSION['role'] == 1 || $post['user_id'] == $user_id): ?>
                             <!-- Only allow the user who created the post or admins to delete -->
                             <form action="delete.php" method="POST" style="display: inline;">
                                 <input type="hidden" name="post_id" value="<?php echo $post['id']; ?>">
